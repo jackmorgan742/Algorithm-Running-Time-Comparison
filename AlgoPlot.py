f9a89912bf0008f678466e585009e947d384c4dd
Jack Morgan <121833759+jackmorgan742@users.noreply.github.com>
2024-02-02 13:05:56 -0500

    Update AlgoPlot.py

--- AlgoPlot.py
+++ AlgoPlot.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 
 # Data
 data_sizes = [10, 100, 1000, 10000, 100000, 1000000]
-brute_force_times = [.002, .003, .023, 5.064, 11.213, 24.112]  # Replace time1, time2, ... with actual times
-binary_search_times = [.002, .003, .003, .032, .035, 1.051]  # Replace time1, time2, ... with actual times
+brute_force_times = [.002, .003, .023, 5.064, 1.234, 24.112]  # Replace time1, time2, ... with actual times
+binary_search_times = [.002, .003, .003, .032, .032, 1.051]  # Replace time1, time2, ... with actual times
 
 # Plot
 plt.plot(data_sizes, brute_force_times, label='Brute Force')
